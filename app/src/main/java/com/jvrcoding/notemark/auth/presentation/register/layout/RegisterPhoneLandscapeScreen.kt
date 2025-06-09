bf61c22316708af5087f90e07d4becd6fe31b9d0
jomarie <jomarierafa1717@gmail.com>
2025-06-09 22:20:22 +0800

    fix keyboard issue in landscape mode

--- app/src/main/java/com/jvrcoding/notemark/auth/presentation/register/layout/RegisterPhoneLandscapeScreen.kt
+++ app/src/main/java/com/jvrcoding/notemark/auth/presentation/register/layout/RegisterPhoneLandscapeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -60,7 +61,8 @@ fun RegisterPhoneLandscapeScreen(
             modifier = Modifier
                 .weight(1f)
                 .verticalScroll(rememberScrollState())
-                .padding(vertical = 32.dp),
+                .padding(vertical = 32.dp)
+                .imePadding(),
             state = state,
             onUsernameChanged = { onUsernameChanged(it) },
             onEmailChanged = { onEmailChanged(it) },
